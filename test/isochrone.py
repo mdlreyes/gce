336a6b48a1ec589e12f90e434f80a6ab746b4f18
mdlreyes <mdelosre@caltech.edu>
2021-06-01 22:30:34 -0400

    fixed typo in isochrone

--- test/isochrone.py
+++ test/isochrone.py
@@ -12,12 +12,22 @@ import matplotlib.pyplot as plt
 # Do some formatting stuff with matplotlib
 from matplotlib import rc
 rc('font', family='serif')
+rc('axes', labelsize=14) 
+rc('xtick', labelsize=10)
+rc('ytick', labelsize=10)
+rc('xtick.major', size=8)
+rc('ytick.major', size=8)
+rc('legend', fontsize=12, frameon=False)
 rc('text',usetex=True)
+rc('xtick',direction='in')
+rc('ytick',direction='in')
+matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
 
 # Import other packages
 import numpy as np
 
 def readlifetimes(grid='dartmouth'):
+    """ Read stellar lifetimes from isochrone grid. """
 
     if grid=='dartmouth':
         # Directory name
@@ -64,28 +74,59 @@ def readlifetimes(grid='dartmouth'):
                                 masses[metal_idx,alpha_idx,agecounter-1] = ln_list[1]
 
                     # Open file with second range of ages (>1 Gyr)
-                    agecounter -= 1
+                    agecounter -= 1  # Don't repeat 1 Gyr
                     with open(filename[:-2],'r') as file:  
                         lines = file.readlines()
                         for idx, line in enumerate(lines):
                             if line != '\n':
                                 if line.startswith('#AGE='):
-                                    print(agecounter)
                                     ages[metal_idx,alpha_idx,agecounter] = float(line[5:11])
                                     agecounter += 1
                             elif lines[idx-1] != '\n':
                                 ln_list = lines[idx-1].split()
-                                print(agecounter-1)
                                 masses[metal_idx,alpha_idx,agecounter-1] = ln_list[1]
                         ln_list = line.split()
-                        masses[0,0,-1] = ln_list[1]
+                        masses[metal_idx,alpha_idx,-1] = ln_list[1]
                 except:
                     pass
 
-    np.save('isochrone_ages', ages)
-    np.save('isochrone_masses', masses)
+    np.save(grid+'_ages', ages)
+    np.save(grid+'_masses', masses)
+
+    return
+
+def plotlifetimes(grid='dartmouth'):
+    """ Plot stellar lifetimes as a function of mass. """
+
+    # Import packages for plotting
+    import cycler
+    import cmasher as cmr
+
+    # Get data
+    ages = np.load(grid+'_ages.npy')
+    masses = np.load(grid+'_masses.npy')
+
+    if grid=='dartmouth':
+        metals = [-2.5,-2.0,-1.5,-1.0,-0.5,0.,0.2,0.3,0.5]  # [Fe/H]
+        alphas = [-0.2,0.,0.2,0.4,0.6,0.8]  # [alpha/Fe]
+
+        # Define colormaps
+        metalcolor = cmr.bubblegum_r(np.linspace(0,1,len(metals),endpoint=True))
+
+        for metal_idx, metal in enumerate(metals):
+            age = ages[metal_idx,0,:]
+            mass = masses[metal_idx,0,:]
+            if np.any(np.isclose(age, 0)) or np.any(np.isclose(mass, 0)):
+                print('test', metal, alphas[0])
+            plt.plot(mass, age, color=metalcolor[metal_idx], label=r"[Fe/H] = {:.2f}".format(metal))
+            print(metal, mass, age)
+        plt.legend()
+        plt.xlabel(r'Mass ($M_{\odot}$)', fontsize=14)
+        plt.ylabel(r'Lifetime (Gyr)', fontsize=14)
+        plt.show()
 
     return
 
 if __name__ == "__main__":
-    readlifetimes()
+    #readlifetimes()
+    plotlifetimes()
