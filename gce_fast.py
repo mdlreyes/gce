added code to plot isochrones, fixed AIC calculation, removed DEIMOSBa from fitting

gce_fast.py
@@ -62,7 +62,7 @@ def runmodel(pars, plot=False, title="", amr=None, sfh=None, empirical=False, em
 
     # Prepare arrays for SNe and AGB calculations
     n_wd = dtd.dtd_ia(t, ia_dtd) * delta_t      # Fraction of stars that will explode as Type Ia SNe in future
-    print('test', n_wd)
+    #print('test', n_wd)
 
     m_himass, n_himass = dtd.dtd_ii(t, imf)       # Mass and fraction of stars that will explode in the future
     goodidx = np.where((m_himass > params.M_SN_min) & (m_himass < params.M_SN_max))[0]  # Limit to timesteps where stars will explode as CCSN
@@ -79,7 +79,7 @@ def runmodel(pars, plot=False, title="", amr=None, sfh=None, empirical=False, em
             n_nsm = dtd.dtd_nsm_enhanced(t) * delta_t      # Fraction of stars that will explode as NSMs
         else:
             n_nsm = dtd.dtd_nsm(t) * delta_t      # Fraction of stars that will explode as NSMs
-        print('test', n_nsm)
+        #print('test', n_nsm)
 
     if empirical==False:
         # Load all sources of chemical yields
@@ -436,6 +436,8 @@ def runmodel(pars, plot=False, title="", amr=None, sfh=None, empirical=False, em
     negll = neglnlike(pars, model=[elem_model, sfr, mstar_model, time, leftovergas])
     aic = 2*k + 2*negll
     bic = np.log(n)*k + 2*negll
+
+    print('test', negll, aic)
 
     return model[:timestep-1], atomic, aic
 

test/isochrone.py
@@ -224,7 +224,98 @@ def plotbpass():
 
     return
 
+def plotisochrones():
+    '''Test function to plot isochrones of 2 different ages from one Dartmouth file.'''
+
+    # Dartmouth isochrones
+    files = ['fehm10afep2.ugriz','fehm15afep2.ugriz']
+    linestyles = ['-','--']
+    titles=['[Fe/H]=-1', '[Fe/H]=-1.5']
+    for i in range(len(files)):
+        with open(files[i],'r') as file:  
+            lines = file.readlines()
+
+            read1 = False
+            read2 = False
+            g1, g2, r1, r2 = [], [], [], []
+            for idx, line in enumerate(lines):
+                if line.startswith('#AGE'):
+                    ln_list = line.split()
+                    if ln_list[0].startswith('#AGE=11.0'):
+                        read1 = True
+                    elif ln_list[0].startswith('#AGE=13.0'):
+                        read2 = True
+                    else:
+                        read1, read2 = False, False
+
+                elif line.startswith('#EEP') or line=='\n':
+                    pass
+
+                else:
+                    ln_list = line.split()
+                    if read1:
+                        g1.append(float(ln_list[6]))
+                        r1.append(float(ln_list[7]))
+                    if read2:
+                        g2.append(float(ln_list[6]))
+                        r2.append(float(ln_list[7]))
+            print(g1)
+
+        plt.plot(np.array(g1)-np.array(r1), np.array(g1), color='k', ls=linestyles[i], label='Dartmouth: 11Gyr, '+titles[i])
+        plt.plot(np.array(g2)-np.array(r2), np.array(g2), color='r', ls=linestyles[i], label='Dartmouth: 13Gyr, '+titles[i])
+
+    # Padova isochrones
+    files = ['isoc_z030_sdss.dat']
+    linestyles = ['--']
+    titles=['[Fe/H]=-1.5']
+    for i in range(len(files)):
+        with open(files[i],'r') as file:  
+            lines = file.readlines()
+
+            read1 = False
+            read2 = False
+            g1, g2, r1, r2 = [], [], [], []
+            for idx, line in enumerate(lines):
+                if line.startswith('#	Isochrone'):
+                    ln_list = line.split()
+                    if ln_list[-2]=='1.122e+10':
+                        read1 = True
+                    elif ln_list[-2]=='1.259e+10':
+                        read2 = True
+                    else:
+                        read1, read2 = False, False
+
+                elif line.startswith('#') or line=='\n':
+                    pass
+
+                else:
+                    ln_list = line.split()
+                    if read1:
+                        g1.append(float(ln_list[8]))
+                        r1.append(float(ln_list[9]))
+                    if read2:
+                        g2.append(float(ln_list[8]))
+                        r2.append(float(ln_list[9]))
+
+            print(g1)
+
+        #plt.plot(np.array(g1)-np.array(r1), np.array(g1), color='gray', ls=linestyles[i], label='Padova: 11Gyr, '+titles[i])
+        plt.plot(np.array(g2)-np.array(r2), np.array(g2), color='pink', ls=linestyles[i], label='Padova: 13Gyr, '+titles[i])
+
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.xlabel(r'$g-r$')
+    plt.ylabel(r'$g$')
+    plt.show()
+
+    #print(np.max(np.abs((np.array(g1)-np.array(r1)) - (np.array(g2)-np.array(r2)))))
+    #print(np.max(np.abs(np.array(g1)-np.array(g2))))
+    #print(np.max(np.abs(np.array(r1)-np.array(r2))))
+        
+    return
+
 if __name__ == "__main__":
     #readlifetimes()
     #plotlifetimes()
-    plotbpass()
+    #plotbpass()
+    plotisochrones()